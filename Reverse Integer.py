f9a13bfb6ad132185a32b991d881fcab24783a0e
kapil120 <sharma.kapil1208@gmail.com>
2020-09-04 20:08:45 +0530

    Reverse Integer

---
+++ Reverse Integer.py
@@ -0,0 +1,37 @@
+7. Reverse Integer
+Easy
+
+3646
+
+5741
+
+Add to List
+
+Share
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+
+Solution:
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        ret, power = 0, 31
+        while n:
+            ret += (n & 1) << power
+            n = n >> 1
+            power -= 1
+        return ret
